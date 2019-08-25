3de2eb6ecd33d5e578fff26c4e81b719588ea3bd
Taylor Thompson <jamestthompson3@gmail.com>
2019-08-25 16:39:23 +0300

    a bit of ui polish

--- api/fetchSrc.js
+++ api/fetchSrc.js
@@ -3,6 +3,10 @@ const prettier = require("prettier");
 
 module.exports = async (req, res) => {
   const fetched = await axios.get(req.body.url);
-  const formatted = prettier.format(fetched.data, { parser: "html" });
-  return res.send(formatted);
+  try {
+    const formatted = prettier.format(fetched.data, { parser: "html" });
+    return res.send(formatted);
+  } catch (e) {
+    return res.send(fetched.data);
+  }
 };
